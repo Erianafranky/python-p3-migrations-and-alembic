454e7c68bc4802c09c5ed62f36b94168d2578d2b
DorothyAriana <arianadottie@gmail.com>
2023-06-02 15:25:13 +0300

    completed assignment

---
+++ app/migrations/versions/a60d4c2a2ead_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a60d4c2a2ead
+Revises: 
+Create Date: 2023-06-02 15:14:27.371911
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a60d4c2a2ead'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
